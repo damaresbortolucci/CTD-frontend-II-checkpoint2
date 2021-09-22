97456b9867bdc72844b9e0a58ef3a050fb5c401d
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2021-09-22 15:59:36 -0300

    correção na função da API

---
+++ script-lista.js
@@ -0,0 +1,97 @@
+let button = document.getElementById('btn')
+let body = document.querySelector('body')
+let inputNovaTarefa = document.getElementById('novaTarefa')
+
+
+
+/* window.onload = () => {
+    carregarAPI();
+}
+
+
+//carregar tarefas na tela com os itens vindos da API
+function carregarAPI(){
+
+   //Pega a ul das tarefas a fazer
+   let ulFazer = document.getElementById('TarefasFazer')
+   //Pega a ul das tarefas a feitas
+   let ulFeitas = document.getElementById('TarefasFeitas')
+
+
+   fetch('https://jsonplaceholder.typicode.com/todos/')
+   .then((response) => response.json())
+       .then((json) => json.map(json => {
+           
+           //cria uma li (item da lista de tarefas)
+           const li = document.createElement('li')
+           li.setAttribute('class', 'tarefa')
+
+           //cria o botao de check que no html/css é uma div estilizada
+           const btnCheck = document.createElement('div')
+           btnCheck.setAttribute('class', 'not-done')
+
+           //cria a div que conterá a descrição da tarefa
+           const divDescricao = document.createElement('div')
+           divDescricao.setAttribute('class', 'descripcion')
+
+           //cria o páragro com descrição da tarefa
+           const p = document.createElement('p')
+           p.setAttribute('class', 'nome' )
+           p.innerHTML = json.title
+
+           //paragrafo que conterá a data de criaçao
+           const p2 = document.createElement('p')
+           p2.setAttribute('class', 'timestamp')
+
+           divDescricao.appendChild(p)
+           divDescricao.appendChild(p2)
+           li.appendChild(btnCheck)
+           li.appendChild(divDescricao)
+
+       
+           //se o atributo completed for false - a tarefa vai para a lista de a fazer
+           if (json.completed == false) {
+               ulFazer.appendChild(li)
+           } else {
+               //se o atributo completed for true - a tarefa vai para a lista de a feito
+               ulFeitas.appendChild(li)
+           }
+        
+       }))
+} */
+
+
+//criar nova tarefa inserida pelo usuario 
+button.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    let ulFazer = document.getElementById('TarefasFazer')
+    let ulFeitas = document.getElementById('TarefasFeitas')
+
+    const li = document.createElement('li')
+    li.setAttribute('class', 'tarefa')
+
+    const btnCheck = document.createElement('div')
+    btnCheck.setAttribute('class', 'not-done')
+
+    const divDescricao = document.createElement('div')
+    divDescricao.setAttribute('class', 'descripcion')
+
+    const p = document.createElement('p')
+    p.setAttribute('class', 'nome' )
+    p.innerHTML = inputNovaTarefa.value;
+    console.log(p)
+
+    const p2 = document.createElement('p')
+    p2.setAttribute('class', 'timestamp')
+
+    divDescricao.appendChild(p)
+    divDescricao.appendChild(p2)
+    li.appendChild(btnCheck)
+    li.appendChild(divDescricao)
+    ulFazer.appendChild(li)
+
+    /* limpa o input para escrever uma nova tarefa */
+    inputNovaTarefa.value="";
+
+}) 
