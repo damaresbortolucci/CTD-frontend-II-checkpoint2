68ed3462bfa017f07234a60f12bcf717b68bbd8a
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2021-09-23 13:10:48 -0300

    incluindo modal

---
+++ script-lista-api.js
@@ -0,0 +1,85 @@
+let button = document.getElementById('btn')
+let body = document.querySelector('body')
+let inputNovaTarefa = document.getElementById('novaTarefa')
+
+let ulFazer = document.getElementById('tarefasFazer')
+
+
+let ulFeitas = document.getElementById('tarefasFeitas')
+let checkFeito = document.getElementById('checkFeito')
+
+
+
+window.onload = () => {
+    carregarAPI();
+}
+
+
+//carregar tarefas na tela com os itens vindos da API
+function carregarAPI(){
+
+   fetch('https://jsonplaceholder.typicode.com/todos')
+   .then((response) => response.json())
+       .then((json) => json.map(json => {
+           
+           //cria uma li (item da lista de tarefas)
+           const li = document.createElement('li')
+           li.setAttribute('class', 'tarefa')
+           li.setAttribute('id', json.id)
+
+           //cria o botao de check que no html/css é uma div estilizada
+           const btnCheck = document.createElement('div')
+           btnCheck.setAttribute('class', 'not-done')
+           btnCheck.setAttribute('onclick', "excluirTarefa("+json.id+")")
+
+           //cria a div que conterá a descrição da tarefa
+           const divDescricao = document.createElement('div')
+           divDescricao.setAttribute('class', 'descripcion')
+
+           const id = document.createElement('p')
+           id.innerHTML = json.id
+
+           //cria o páragro com descrição da tarefa
+           const p = document.createElement('p')
+           p.setAttribute('class', 'nome' )
+           p.innerHTML = json.title
+
+           //paragrafo que conterá a data de criaçao
+           const p2 = document.createElement('p')
+           p2.setAttribute('class', 'timestamp')
+
+           divDescricao.appendChild(id)
+           divDescricao.appendChild(p)
+           divDescricao.appendChild(p2)
+           li.appendChild(btnCheck)
+           li.appendChild(divDescricao)
+
+       
+           //se o atributo completed for false - a tarefa vai para a lista de a fazer
+           if (json.completed == false) {
+               ulFazer.appendChild(li)
+           } else {
+               //se o atributo completed for true - a tarefa vai para a lista de a feito
+               ulFeitas.appendChild(li)
+           }
+        
+       }))
+}
+
+
+    function excluirTarefa(id){
+
+       fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => response.json())
+            .then((json) => json.map(json => {
+           
+            let li = document.getElementById(id)
+
+            if(json.id = id){
+                if(json.completed == false){
+                    ulFeitas.appendChild(li)
+                }
+            }  
+       })) 
+    }
+
--- script-lista.js
+++ script-lista.js
@@ -2,68 +2,19 @@ let button = document.getElementById('btn')
 let body = document.querySelector('body')
 let inputNovaTarefa = document.getElementById('novaTarefa')
 let inputDataDeTermino = document.getElementById('dataDeTermino')
+let ulFazer = document.getElementById('TarefasFazer')
+let ulFeitas = document.getElementById('TarefasFeitas')
+
+let indexCard=0; //contador do id
+let arrayTarefas = []; //array onde armazenamos cada tarefa que é um objeto
 
 
-/* window.onload = () => {
-    carregarAPI();
-}
-
-
-//carregar tarefas na tela com os itens vindos da API
-function carregarAPI(){
-
-   //Pega a ul das tarefas a fazer
-   let ulFazer = document.getElementById('TarefasFazer')
-   //Pega a ul das tarefas a feitas
-   let ulFeitas = document.getElementById('TarefasFeitas')
-
-
-   fetch('https://jsonplaceholder.typicode.com/todos/')
-   .then((response) => response.json())
-       .then((json) => json.map(json => {
-           
-           //cria uma li (item da lista de tarefas)
-           const li = document.createElement('li')
-           li.setAttribute('class', 'tarefa')
-
-           //cria o botao de check que no html/css é uma div estilizada
-           const btnCheck = document.createElement('div')
-           btnCheck.setAttribute('class', 'not-done')
-
-           //cria a div que conterá a descrição da tarefa
-           const divDescricao = document.createElement('div')
-           divDescricao.setAttribute('class', 'descripcion')
-
-           //cria o páragro com descrição da tarefa
-           const p = document.createElement('p')
-           p.setAttribute('class', 'nome' )
-           p.innerHTML = json.title
-
-           //paragrafo que conterá a data de criaçao
-           const p2 = document.createElement('p')
-           p2.setAttribute('class', 'timestamp')
-
-           divDescricao.appendChild(p)
-           divDescricao.appendChild(p2)
-           li.appendChild(btnCheck)
-           li.appendChild(divDescricao)
-
-       
-           //se o atributo completed for false - a tarefa vai para a lista de a fazer
-           if (json.completed == false) {
-               ulFazer.appendChild(li)
-           } else {
-               //se o atributo completed for true - a tarefa vai para a lista de a feito
-               ulFeitas.appendChild(li)
-           }
-        
-       }))
-} */
 
 
 //criar nova tarefa inserida pelo usuario 
 button.addEventListener('click', function(event) {
     event.preventDefault()
+
     
     //validação se está preenchendo o campo de tarfa
     if(inputNovaTarefa.value == ""){
@@ -84,47 +35,80 @@ button.addEventListener('click', function(event) {
     else{
 
 
-        //
-    let ulFazer = document.getElementById('TarefasFazer')
-    let ulFeitas = document.getElementById('TarefasFeitas')
+        indexCard+=1; //contador para o id
+        //cria o arrayTarefas no localstarogage
+        localStorage.setItem('tarefas', JSON.stringify(arrayTarefas));
+        //recupera o array do localstorage e armazena numa variavel
+        let getObj = JSON.parse(localStorage.getItem('tarefas'));
 
 
-    const li = document.createElement('li')
-    li.setAttribute('class', 'tarefa')
-
-    const btnCheck = document.createElement('div')
-    btnCheck.setAttribute('class', 'not-done')
-
-    const divDescricao = document.createElement('div')
-    divDescricao.setAttribute('class', 'descripcion')
-
-    const p = document.createElement('p')
-    p.setAttribute('class', 'nome' )
-    p.innerHTML = inputNovaTarefa.value;
-    console.log(p)
-
-    const p2 = document.createElement('p')
-    p2.setAttribute('class', 'timestamp')
-
-    divDescricao.appendChild(p)
-    divDescricao.appendChild(p2)
-    li.appendChild(btnCheck)
-    li.appendChild(divDescricao)
-    ulFazer.appendChild(li)
-
+        const li = document.createElement('li')
+        li.setAttribute('class', 'tarefa')
+        li.setAttribute('id', indexCard)
     
-    /* limpa o input para escrever uma nova tarefa */
-    inputNovaTarefa.value="";
-    inputDataDeTermino.value="";
+        const btnNotDone = document.createElement('div')
+        btnNotDone.setAttribute('class', 'not-done')
+        btnNotDone.setAttribute('onclick', 'mostrarModal('+indexCard+')')
+    
+        const divDescricao = document.createElement('div')
+        divDescricao.setAttribute('class', 'descripcion')
+    
+        const p = document.createElement('p')
+        p.setAttribute('class', 'nome' )
+        p.innerHTML = inputNovaTarefa.value;
+     
+        const dataEcheck = document.createElement('div')
+        dataEcheck.setAttribute('class', 'dataEcheck')
+    
+        const p2 = document.createElement('p')
+        p2.setAttribute('class', 'timestamp')
+    
+        const inputCheckBox = document.createElement('input')
+        inputCheckBox.setAttribute('type', 'checkbox')
+    
+        dataEcheck.appendChild(p2)
+        dataEcheck.appendChild(inputCheckBox)
+        divDescricao.appendChild(p)
+        divDescricao.appendChild(dataEcheck)
+        li.appendChild(btnNotDone)
+        li.appendChild(divDescricao)
+        ulFazer.appendChild(li)
 
+        
+
+        //cria um novo objeto guardando o ID e o texto da tarefa
+        let newObj = {"id": indexCard, "tarefa": inputNovaTarefa.value}
+    
+        //adiciona esse novo objeto ao array de tarefas
+        arrayTarefas.push(newObj)
+    
+        // Insere o array convertido em JSON no localStorage 
+        localStorage.setItem('tarefas', JSON.stringify(arrayTarefas));
+
+
+        /* limpa o input para escrever uma nova tarefa */
+        inputNovaTarefa.value="";
+        inputDataDeTermino.value="";
 
     }    
     }
     }
 }) 
 
+
+
+function mostrarModal(){
+    let el = document.getElementById('deleteModal')
+    let minha_modal = new bootstrap.Modal(el)
+    minha_modal.show()
+  }
+
+
+
+  function deletar(){
+    /* falta implementar */
+  }
+
+
 //validação de campos vazios
-
-
 //let validarTarefaPreenchido = document.getElementById('novaTarefa')
-
