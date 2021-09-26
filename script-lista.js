5078b5d8977efc94f04bdb8ab5ea256a13232b77
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2021-09-25 23:45:56 -0300

    exclusao de cards

--- script-lista.js
+++ script-lista.js
@@ -4,10 +4,91 @@ let inputNovaTarefa = document.getElementById('novaTarefa')
 let inputDataDeTermino = document.getElementById('dataDeTermino')
 let ulFazer = document.getElementById('TarefasFazer')
 let ulFeitas = document.getElementById('TarefasFeitas')
-
 let indexCard=0; //contador do id
-let arrayTarefas = []; //array onde armazenamos cada tarefa que é um objeto
 
+
+window.onload = _ => {
+
+    /* recupera array de tarefas a fazer */
+    let arrayTarefas = JSON.parse(localStorage.getItem('tarefas'));
+    /* recupera array de tarefas a feitas */
+    let arrayFeitas =  JSON.parse(localStorage.getItem('tarefasFeitas'));
+
+    //recria cards das tarefas a fazer na tela
+    if(arrayTarefas != null){
+        indexCard =  arrayTarefas.lastIndex
+        //Percorre array do local storage para criar cards 
+        arrayTarefas.forEach((element) => {
+            criarCards(element.id, element.tarefa);
+        });
+    }
+
+    //recria cards das tarefas feitas e  riscadas pelo checkbox
+    if(arrayFeitas != null){
+        //Percorre array do local storage para criar cards 
+        arrayFeitas.forEach((element) => {
+            criarCardFeitas(element.tarefa);
+        });
+    }
+}
+
+
+
+//CRIAR NOVOS CARDS
+function criarCards(id, input){
+
+    const li = document.createElement('li')
+    li.setAttribute('class', 'tarefa tarefaFazer')
+    li.setAttribute('id', id)
+
+    const btnNotDone = document.createElement('div')
+    btnNotDone.setAttribute('class', 'not-done')
+    btnNotDone.setAttribute('onclick', 'mostrarModal('+id+')')
+
+    const divDescricao = document.createElement('div')
+    divDescricao.setAttribute('class', 'descripcion')
+
+    const p = document.createElement('p')
+    p.setAttribute('class', 'nome' )
+    p.innerHTML = input
+ 
+    const dataEcheck = document.createElement('div')
+    dataEcheck.setAttribute('class', 'dataEcheck')
+
+    const p2 = document.createElement('p')
+    p2.setAttribute('class', 'timestamp')
+
+    const inputCheckBox = document.createElement('input')
+    inputCheckBox.setAttribute('type', 'checkbox')
+    inputCheckBox.setAttribute('id', 'checkbox')
+    inputCheckBox.setAttribute('onclick', 'riscar('+id+')')
+
+    dataEcheck.appendChild(p2)
+    dataEcheck.appendChild(inputCheckBox)
+    divDescricao.appendChild(p)
+    divDescricao.appendChild(dataEcheck)
+    li.appendChild(btnNotDone)
+    li.appendChild(divDescricao)
+    ulFazer.appendChild(li)
+   
+}
+
+//CRIAR CARDS DE TAREFAS RISCADAS COM O CHECKBOX (FEITAS)
+function criarCardFeitas(input){
+
+    ulFeitas.innerHTML += `
+    <li class="tarefa">
+            <div class="not-done"></div>
+            <div class="descripcion">
+                <p class="nome">${input}</p>
+                <div class="dataEcheck">
+                    <p class="timestamp">Criada: 19/04/20</p>
+                    <input type="checkbox" name="" id="">
+                </div>
+            </div>
+        </li>
+    `
+}
 
 
 
@@ -15,83 +96,72 @@ let arrayTarefas = []; //array onde armazenamos cada tarefa que é um objeto
 button.addEventListener('click', function(event) {
     event.preventDefault()
 
-    
     //validação se está preenchendo o campo de tarfa
     if(inputNovaTarefa.value == ""){
         alert("Escreva a tarefa antes de envia-la")
     }
-    else{
-
-        //validação se a descrição da tarefa possui mais de 10 chars
-    if(inputNovaTarefa.value.length < 10 && inputNovaTarefa.value != ""){
+    else if(inputNovaTarefa.value.length < 10 && inputNovaTarefa.value != ""){
         alert("A descrição da tarefa precisa ter 10 caracters")
     }
-    else{
-
-        //validação data de termino
-    if(inputDataDeTermino.value == ""){
+    else if(inputDataDeTermino.value == ""){
         alert("Preencha a data de termino da tarefa antes de envia-la.")
     }
     else{
 
+        let arrayObjetos = [];
+        indexCard+=1;
+        criarCards(indexCard, inputNovaTarefa.value)
 
-        indexCard+=1; //contador para o id
-        //cria o arrayTarefas no localstarogage
-        localStorage.setItem('tarefas', JSON.stringify(arrayTarefas));
-        //recupera o array do localstorage e armazena numa variavel
+
+        //pega os dados do localstorage
         let getObj = JSON.parse(localStorage.getItem('tarefas'));
 
+        //se já existirem tarefas no localstorage
+        if(getObj != null) {
 
-        const li = document.createElement('li')
-        li.setAttribute('class', 'tarefa')
-        li.setAttribute('id', indexCard)
+            
+            let getObj = JSON.parse(localStorage.getItem('tarefas'));
+            //Recupera id atual no lastIndex 
+            indexCard =  getObj[0].lastIndex + 1;
     
-        const btnNotDone = document.createElement('div')
-        btnNotDone.setAttribute('class', 'not-done')
-        btnNotDone.setAttribute('onclick', 'mostrarModal('+indexCard+')')
+            //cria o card da tarefa que usuario esta escolhendo
+            let newObj = {"id": indexCard, "tarefa": inputNovaTarefa.value, "lastIndex": indexCard}
     
-        const divDescricao = document.createElement('div')
-        divDescricao.setAttribute('class', 'descripcion')
+            /* Todos os obj recebem o lastindex o ultimo ID sera recuperado */
+            getObj.forEach(element => {
+                element.lastIndex = indexCard;
+                arrayObjetos.push(element);
+            });
     
-        const p = document.createElement('p')
-        p.setAttribute('class', 'nome' )
-        p.innerHTML = inputNovaTarefa.value;
-     
-        const dataEcheck = document.createElement('div')
-        dataEcheck.setAttribute('class', 'dataEcheck')
+            /* Insere dados do novo card no array de objetos temporario */
+            arrayObjetos.push(newObj)
     
-        const p2 = document.createElement('p')
-        p2.setAttribute('class', 'timestamp')
+            /* Apaga localStorage para receber array atualizado com cards antigos e novos */
+            localStorage.removeItem('tarefas');
     
-        const inputCheckBox = document.createElement('input')
-        inputCheckBox.setAttribute('type', 'checkbox')
-    
-        dataEcheck.appendChild(p2)
-        dataEcheck.appendChild(inputCheckBox)
-        divDescricao.appendChild(p)
-        divDescricao.appendChild(dataEcheck)
-        li.appendChild(btnNotDone)
-        li.appendChild(divDescricao)
-        ulFazer.appendChild(li)
+            /* Insere o array temporario convertido em JSON no localStorage */
+            localStorage.setItem('tarefas', JSON.stringify(arrayObjetos));
 
-        
+        } else {
+            arrayObjetos.push(
+                {
+                    id: indexCard,
+                    tarefa: inputNovaTarefa.value,
+                    lastIndex: indexCard
+                }
+            );
 
-        //cria um novo objeto guardando o ID e o texto da tarefa
-        let newObj = {"id": indexCard, "tarefa": inputNovaTarefa.value}
+            /* Apaga localStorage para receber array atualizado com cards antigos e novos */
+            localStorage.removeItem('tarefas');
     
-        //adiciona esse novo objeto ao array de tarefas
-        arrayTarefas.push(newObj)
-    
-        // Insere o array convertido em JSON no localStorage 
-        localStorage.setItem('tarefas', JSON.stringify(arrayTarefas));
-
+            /* Insere o array temporario convertido em JSON no localStorage */
+            localStorage.setItem('tarefas', JSON.stringify(arrayObjetos));
+            indexCard = arrayObjetos[0].id;
+        }    
 
         /* limpa o input para escrever uma nova tarefa */
         inputNovaTarefa.value="";
         inputDataDeTermino.value="";
-
-    }    
-    }
     }
 }) 
 
@@ -109,15 +179,72 @@ function mostrarModal(id){
 
 
 
+  //deletar um card pelo botao excluir do modal
   function deletar(id){
-    const li = document.querySelectorAll("li")
-    li.forEach(el=>{
-        if(el.getAttribute("id") == id) {
-            el.style.display = "none"
-        }
-    })
+    let arrayTarefas = JSON.parse(localStorage.getItem('tarefas'))
+    let index = arrayTarefas.findIndex(element => element.id == id)
+
+    if(arrayTarefas.length > 1) {
+        arrayTarefas.splice(index, 1)
+        let arrayJson = JSON.stringify(arrayTarefas)
+        localStorage.setItem('tarefas', arrayJson)
+    } else {
+        localStorage.removeItem('tarefas');
+    }
+    
+    // recarrega a página após a exclusão do card
+    document.location.reload(true);
   }
 
 
-//validação de campos vazios
-//let validarTarefaPreenchido = document.getElementById('novaTarefa')
+
+
+  // riscar card pelo checkbox e passar para a lista de tarefas feitas abaixo
+function riscar(id){
+    let checkbox = document.querySelectorAll('#checkbox')
+    let ulPendentes = document.querySelectorAll('.tarefaFazer')
+    let listaTarefas = JSON.parse(localStorage.getItem('tarefas'))
+    
+    for(let i=0; i < ulPendentes.length; i++){
+        if(checkbox[i].checked){
+        
+            ulFeitas.appendChild(ulPendentes[i])  
+
+
+
+           //atualizar cards de tarefas feitas 
+           /* let arrayFeitas =  JSON.parse(localStorage.getItem('tarefasFeitas'));
+
+            if(arrayFeitas != null){
+                let newObj = {"tarefa": ulPendentes[i].tarefa}
+                arrayFeitas.push(newObj)
+                localStorage.removeItem('tarefasFeitas');
+                localStorage.setItem('tarefasFeitas', JSON.stringify(arrayFeitas));
+            }else{
+
+                let tarefaFeita = [];
+                tarefaFeita.push(
+                    {tarefa: listaTarefas[i].tarefa}
+                    )
+                localStorage.removeItem('tarefasFeitas');
+                localStorage.setItem('tarefasFeitas', JSON.stringify(tarefaFeita));
+            }
+
+
+           //atualizar os cards de tarefas a fazer 
+           let arrayTarefas = JSON.parse(localStorage.getItem('tarefas'))
+           let index = arrayTarefas.findIndex(element => element.id == id)
+
+           if(arrayTarefas.length > 1) {
+                arrayTarefas.splice(index, 1)
+                let arrayJson = JSON.stringify(arrayTarefas)
+                localStorage.setItem('tarefas', arrayJson)
+            } else {
+                localStorage.removeItem('tarefas');
+            } */
+        }
+        
+    }
+    /* document.location.reload(true); */
+}
+
