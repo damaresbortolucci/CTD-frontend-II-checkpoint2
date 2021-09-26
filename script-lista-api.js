ffed85dc15326590b78b41029480881afbb2616a
silasms <silas.medeiros7@gmail.com>
2021-09-25 21:42:46 -0300

    Implementação de imagens aleatórias

--- script-lista-api.js
+++ script-lista-api.js
@@ -16,70 +16,69 @@ window.onload = () => {
 
 
 //carregar tarefas na tela com os itens vindos da API
-function carregarAPI(){
+function carregarAPI() {
 
-   fetch('https://jsonplaceholder.typicode.com/todos')
-   .then((response) => response.json())
-       .then((json) => json.map(json => {
-           
-           //cria uma li (item da lista de tarefas)
-           const li = document.createElement('li')
-           li.setAttribute('class', 'tarefa')
-           li.setAttribute('id', json.id)
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => response.json())
+        .then((json) => json.map(json => {
 
-           //cria o botao de check que no html/css é uma div estilizada
-           const btnCheck = document.createElement('div')
-           btnCheck.setAttribute('class', 'not-done')
-           btnCheck.setAttribute('onclick', "excluirTarefa("+json.id+")")
+            //cria uma li (item da lista de tarefas)
+            const li = document.createElement('li')
+            li.setAttribute('class', 'tarefa')
+            li.setAttribute('id', json.id)
 
-           //cria a div que conterá a descrição da tarefa
-           const divDescricao = document.createElement('div')
-           divDescricao.setAttribute('class', 'descripcion')
+            //cria o botao de check que no html/css é uma div estilizada
+            const btnCheck = document.createElement('div')
+            btnCheck.setAttribute('class', 'not-done')
+            btnCheck.setAttribute('onclick', "excluirTarefa(" + json.id + ")")
 
-           const id = document.createElement('p')
-           id.innerHTML = json.id
+            //cria a div que conterá a descrição da tarefa
+            const divDescricao = document.createElement('div')
+            divDescricao.setAttribute('class', 'descripcion')
 
-           //cria o páragro com descrição da tarefa
-           const p = document.createElement('p')
-           p.setAttribute('class', 'nome' )
-           p.innerHTML = json.title
+            const id = document.createElement('p')
+            id.innerHTML = json.id
 
-           //paragrafo que conterá a data de criaçao
-           const p2 = document.createElement('p')
-           p2.setAttribute('class', 'timestamp')
+            //cria o páragro com descrição da tarefa
+            const p = document.createElement('p')
+            p.setAttribute('class', 'nome')
+            p.innerHTML = json.title
 
-           divDescricao.appendChild(id)
-           divDescricao.appendChild(p)
-           divDescricao.appendChild(p2)
-           li.appendChild(btnCheck)
-           li.appendChild(divDescricao)
+            //paragrafo que conterá a data de criaçao
+            const p2 = document.createElement('p')
+            p2.setAttribute('class', 'timestamp')
 
-       
-           //se o atributo completed for false - a tarefa vai para a lista de a fazer
-           if (json.completed == false) {
-               ulFazer.appendChild(li)
-           } else {
-               //se o atributo completed for true - a tarefa vai para a lista de a feito
-               ulFeitas.appendChild(li)
-           }
-        
-       }))
+            divDescricao.appendChild(id)
+            divDescricao.appendChild(p)
+            divDescricao.appendChild(p2)
+            li.appendChild(btnCheck)
+            li.appendChild(divDescricao)
+
+
+            //se o atributo completed for false - a tarefa vai para a lista de a fazer
+            if (json.completed == false) {
+                ulFazer.appendChild(li)
+            } else {
+                //se o atributo completed for true - a tarefa vai para a lista de a feito
+                ulFeitas.appendChild(li)
+            }
+
+        }))
 }
 
 
-    function excluirTarefa(id){
-
-       fetch('https://jsonplaceholder.typicode.com/todos')
+function excluirTarefa(id) {
+    fetch('https://jsonplaceholder.typicode.com/todos')
         .then((response) => response.json())
-            .then((json) => json.map(json => {
-           
+        .then((json) => json.map(json => {
+
             let li = document.getElementById(id)
 
-            if(json.id = id){
-                if(json.completed == false){
+            if (json.id = id) {
+                if (json.completed == false) {
                     ulFeitas.appendChild(li)
                 }
-            }  
-       })) 
-    }
+            }
+        }))
+}
 
--- script-login.js
+++ script-login.js
@@ -1,4 +1,3 @@
-
 let button = document.querySelector('button')
 
 button.addEventListener('click', function (event) {
@@ -17,59 +16,11 @@ button.addEventListener('click', function (event) {
         }))
 })
 
-
-
-let c = document.getElementById("myCanvas");
-let ctx = c.getContext("2d");
-
-let numLoad = 0.02
-
-function loading() {
-    return new Promise(function (resolve, reject) {
-        setTimeout(_ => {
-            if (numLoad <= 2) {
-                resolve(numLoad += 0.02)
-            } else {
-                reject(console.log("Err"))
-            }
-        }, 16)
+axios.get("https://dog.ceo/api/breeds/image/random")
+    .then(url => {
+        console.log(url.data)
+        const images = document.querySelector(".image")
+        const img = document.createElement("img")
+        images.appendChild(img)
+        img.setAttribute("src", url.data.message)
     })
-}
-async function inLoad() {
-    c.width = 300
-    c.height = 150
-    for (let i = 0; i < 100; i++) {
-        await loading()
-        ctx.beginPath();
-        ctx.lineWidth = 4;
-        ctx.clearRect(0, 0, c.width, c.height);
-        ctx.arc(150, 75, 20, 0, numLoad * Math.PI);
-        ctx.strokeStyle = "#eaeaea";
-        ctx.stroke()
-        document.querySelector(".percent").innerHTML = i + 2
-        if (numLoad >= 2) {
-            const dots = document.querySelector(".dots")
-            setTimeout(function () {
-                let form = document.querySelector("form")
-                form.style.filter = "none";
-                ctx.clearRect(0, 0, c.width, c.height);
-                c.style.diplay = "none"
-                document.querySelectorAll("input").forEach(el => el.disabled = false)
-                document.querySelector(".blur").style.display = "none"
-                document.querySelector(".percent").style.display = "none"
-                dots.style.transition = "none"
-                dots.style.color = "transparent"
-            }, 1000)
-            for (let v = 1; v < 3; v++) {
-                if(v==1) dots.style.color = "#c9c9c9"
-                setTimeout(function () {
-                    dots.innerHTML += "<span></span>."
-                }, 250*v)
-            }
-        }
-    }
-}
-
-window.onload = _ => {
-    inLoad()
-}
